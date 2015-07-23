f86d0e3958217d26b8a6a84867773e29ea5f7a98
MKS-students <mks.students@hackreactor.com>
2015-07-23 12:24:01 -0700

    [progress] saving new sites completed and tested

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -13,8 +14,9 @@ var websites;
 
 exports.paths = {
   siteAssets: path.join(__dirname, '../web/public'),
-  archivedSites: path.join(__dirname, '../archives/sites'),
-  list: path.join(__dirname, '../archives/sites.txt')
+  archivedSites: path.join(__dirname, '../archives/sites'), /// maybe add /
+  list: path.join(__dirname, '../archives/sites.txt'),
+  main: path.join(__dirname, '../web/public/index.html'),
 };
 
 // Used for stubbing paths for tests, do not modify
@@ -24,42 +26,44 @@ exports.initialize = function(pathsObj){
   });
 };
 
-// The following function names are provided to you to suggest how you might
-// modularize your code. Keep it clean!
-
-exports.readListOfUrls = function(cb){
-	// get websites from server
-	fs.readFile(../archives/sites.txt, function(err, websites){
+exports.readListOfUrls = function(targetUrl, cb){
+	fs.readFile(exports.paths.list, 'utf8', function(err, websites){
 		if (err) {console.log("error in readListOfUrls")}
-    cb(websites);
-	})
-
-};
-
-exports.isUrlInList = function(targetUrl, cb){
-  readListOfUrls(function(targetUrl, listOfWebsites){
-  	listOfWebsites = listOfWebsites.split('\n');
-  	cb(listOfWebsites.indexOf(targetUrl) !== -1);
-  })
-
-};
-
-exports.addUrlToList = function(url){
-	fs.appendFile('../archives/sites.txt', url+ '\n', function (err) {
-	  if (err) {
-	  	throw err;
-	  }
-	  console.log( url, ' was appended to the list of sites!');
+		console.log('websites', websites);
+    cb(targetUrl, websites);
 	});
 };
 
-exports.isUrlArchived = function(url, cb){
-	fs.exists("../archives/sites/"+url, function(doesExist){
-		cb(doesExist);
-	})
-	///how's that name?
+exports.isUrlInList = function(targetUrl, cb){
+  exports.readListOfUrls(targetUrl, function(targetUrl, listOfWebsites){
+  	listOfWebsites = listOfWebsites.split('\n');
+  	cb(listOfWebsites.indexOf(targetUrl) !== -1);
+  });
 };
 
-exports.downloadUrls = function(){
+exports.addUrlToList = function(targetUrl){
+	fs.appendFile(exports.paths.list, targetUrl+ '\n', function (err) {
+	  if (err) {
+	  	throw err;
+	  }
+	  console.log( targetUrl, ' was appended to the list of sites!');
+	});
+};
 
+exports.isUrlArchived = function(targetUrl, cb){
+	fs.exists(exports.paths.archivedSites+targetUrl, function(doesExist){
+		cb(doesExist);
+	});
+};
+
+exports.downloadUrls = function(targetUrl){
+	request(targetUrl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      fs.writeFile(targetUrl, body, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+			});
+    }
+  });
 };
